73b0c1048bc5a12608a94767dafb7a500c86102c
Amir Shiri <15230040+Gorthog@users.noreply.github.com>
2025-08-07 21:21:36 +0300

    Update common.js

--- he/common.js
+++ he/common.js
@@ -1,3 +1,9 @@
 {
-  "signIn": "התחבר"
+  "signIn": "התחבר",
+  "login": "התחבר",
+  "email": "אימייל",
+  "password": "סיסמא",
+  "forgotPassword": "שכחת סיסמא?",
+  "selectAnotherUser": "התחבר עם משתמש אחר",
+  "termsAndConditions": "תנאי שימוש"
 }
