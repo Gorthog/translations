28991340ba36fbea879deb87e4967ec9a8ee789c
Amir Shiri <15230040+Gorthog@users.noreply.github.com>
2025-08-07 21:21:08 +0300

    Update common.js

--- en/common.js
+++ en/common.js
@@ -1,3 +1,9 @@
 {
-  "signIn": "Sign in"
+  "signIn": "Sign in",
+  "login": "Login",
+  "email": "Email",
+  "password": "Password",
+  "forgotPassword": "Forgot password?",
+  "selectAnotherUser": "Sign in with another user",
+  "termsAndConditions": "Terms & Conditions"
 }
